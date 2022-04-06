f87a7304fdf30a821d017039998735b82c82de5b
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-04-06 09:55:04 +0200

    base form finished

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+  <h1>Todo App</h1>
+<form>
+  <div class="row">
+    <input class="text" type="text" /> 
+    <button class="btnAdd">Add Item</button>
+  </div>
+  <div className="tasks"></div>
+</form>
+`;
+
+
