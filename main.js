0377b2cdbcd69a4eb711b2b2f8c0aedaf3f8a28d
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-04-06 10:10:30 +0200

    two dummy tasks

--- main.js
+++ main.js
@@ -15,12 +15,26 @@ const textElem = document.querySelector('.text');
 const btnAddElem = document.querySelector('.btnAdd');
 const tasksElem = document.querySelector('.tasks');
 
+const getTasks = () => {
+  return [
+    {
+      text: "task 1"
+    },
+    {
+      text: "task 2"
+    }
+  ]
+};
+
 const renderList = () => {
-  const taskElem = document.createElement('div');
-  console.log(taskElem);
-  taskElem.innerHTML = 'this is a task';
-  taskElem.classList.add('task');
-  tasksElem.appendChild(taskElem);
+  const tasks = getTasks();
+  tasks.forEach(task => {
+    const taskElem = document.createElement('div');
+    taskElem.innerHTML = task.text;
+    taskElem.classList.add('task');
+    tasksElem.appendChild(taskElem);
+
+  });
 }
 
 
