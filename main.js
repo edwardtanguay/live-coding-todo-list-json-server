f5acc6816ea23b11ff3ac196750106028db8701b
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-04-07 09:19:18 +0200

    prettier changes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './style.css';
 
 document.querySelector('#app').innerHTML = `
   <h1>Todo App</h1>
@@ -16,75 +16,86 @@ const btnAddElem = document.querySelector('.btnAdd');
 const tasksElem = document.querySelector('.tasks');
 
 const getTasks = async () => {
-  const response = await fetch('http://localhost:5011/todos');
-  const data = await response.json();
-  return data;
+	const response = await fetch('http://localhost:5011/todos');
+	const data = await response.json();
+	return data;
 };
 
 const renderList = async () => {
-  tasksElem.innerHTML = '';
-  const tasks = await getTasks();
-  tasks.forEach(task => {
-
-    const taskElem = document.createElement('div');
-    taskElem.setAttribute('data-id', task.id);
-    taskElem.setAttribute('data-finished', task.finished);
-    taskElem.innerHTML = `
-   <input type="checkbox" ${task.finished ? 'checked' : ''} class="toggleFinished"/> 
+	tasksElem.innerHTML = '';
+	const tasks = await getTasks();
+	tasks.forEach((task) => {
+		const taskElem = document.createElement('div');
+		taskElem.setAttribute('data-id', task.id);
+		taskElem.setAttribute('data-finished', task.finished);
+		taskElem.innerHTML = `
+   <input type="checkbox" ${
+		task.finished ? 'checked' : ''
+   } class="toggleFinished"/> 
    <div>${task.text}</div>
    <button class="btnDelete">delete</button>
     `;
-    taskElem.classList.add('task');
-    tasksElem.appendChild(taskElem);
-  });
-  textElem.value = '';
-  textElem.focus();
+		taskElem.classList.add('task');
+		tasksElem.appendChild(taskElem);
+	});
+	textElem.value = '';
+	textElem.focus();
 
-  // delete button events
-  const deleteButtonElems = document.querySelectorAll('.btnDelete');
-  deleteButtonElems.forEach(m => m.addEventListener('click', async (e) => {
-    e.preventDefault();
-    const currentTaskElem = m.parentElement;
-    const currentId = currentTaskElem.dataset.id;
-    const requestOptions = {
-      method: 'DELETE'
-    };
-    const response = await fetch(`http://localhost:5011/todos/${currentId}`, requestOptions);
-    renderList();
-  }));
+	// delete button events
+	const deleteButtonElems = document.querySelectorAll('.btnDelete');
+	deleteButtonElems.forEach((m) =>
+		m.addEventListener('click', async (e) => {
+			e.preventDefault();
+			const currentTaskElem = m.parentElement;
+			const currentId = currentTaskElem.dataset.id;
+			const requestOptions = {
+				method: 'DELETE',
+			};
+			const response = await fetch(
+				`http://localhost:5011/todos/${currentId}`,
+				requestOptions
+			);
+			renderList();
+		})
+	);
 
-  // checkbox events
-  const checkboxElems = document.querySelectorAll('.toggleFinished');
-  checkboxElems.forEach(m => m.addEventListener('change', async () => {
-    const currentTaskElem = m.parentElement;
-    const currentId = currentTaskElem.dataset.id;
-    const currentFinished = currentTaskElem.dataset.finished === 'true' ? true : false;
-    const requestOptions = {
-      method: 'PATCH',
-      body: JSON.stringify({ finished: !currentFinished}),
-      headers: { "Content-type": "application/json; charset=UTF-8" }
-    };
-    const response = await fetch(`http://localhost:5011/todos/${currentId}`, requestOptions);
-    renderList();
-  }));
-
+	// checkbox events
+	const checkboxElems = document.querySelectorAll('.toggleFinished');
+	checkboxElems.forEach((m) =>
+		m.addEventListener('change', async () => {
+			const currentTaskElem = m.parentElement;
+			const currentId = currentTaskElem.dataset.id;
+			const currentFinished =
+				currentTaskElem.dataset.finished === 'true' ? true : false;
+			const requestOptions = {
+				method: 'PATCH',
+				body: JSON.stringify({ finished: !currentFinished }),
+				headers: { 'Content-type': 'application/json; charset=UTF-8' },
+			};
+			const response = await fetch(
+				`http://localhost:5011/todos/${currentId}`,
+				requestOptions
+			);
+			renderList();
+		})
+	);
 };
 
 renderList();
 
 btnAddElem.addEventListener('click', async (e) => {
-  e.preventDefault();
+	e.preventDefault();
 
-  const newTodo = {
-    "text": textElem.value,
-    "finished": false
-  };
+	const newTodo = {
+		text: textElem.value,
+		finished: false,
+	};
 
-  const requestOptions = {
-    method: 'POST',
-    body: JSON.stringify(newTodo),
-    headers: { "Content-type": "application/json; charset=UTF-8" }
-  };
-  const response = await fetch('http://localhost:5011/todos', requestOptions);
-  renderList();
+	const requestOptions = {
+		method: 'POST',
+		body: JSON.stringify(newTodo),
+		headers: { 'Content-type': 'application/json; charset=UTF-8' },
+	};
+	const response = await fetch('http://localhost:5011/todos', requestOptions);
+	renderList();
 });
